eb60600ee770e55ff2f956ea39a8554c4ea91aca
Jacob Arriola <jacob@zeek.com>
2019-10-20 16:50:10 -0700

    init serverless functions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const fetch = require('isomorphic-unfetch')
 module.exports = {
   async exportPathMap () {
     const paths = {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/spotify': { page: '/spotify' }
     }
 
     const res = await fetch(`https://cdn.contentful.com/spaces/4pywjkutx049/environments/master/entries?access_token=a70d2276fb3f46ebc664b8aeab91d5cc7ee6ef7f9b6d2ce0aa8bdc56abb2d6b3`)
