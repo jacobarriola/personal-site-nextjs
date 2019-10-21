3a1b5a9469b8cef20cac7c2fc483762f65d587df
Jacob Arriola <jacob@zeek.com>
2019-10-21 06:15:52 -0700

    Fix export. Ensure blogPosts is content type

--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ module.exports = {
       '/spotify': { page: '/spotify' }
     }
 
-    const res = await fetch(`https://cdn.contentful.com/spaces/4pywjkutx049/environments/master/entries?access_token=a70d2276fb3f46ebc664b8aeab91d5cc7ee6ef7f9b6d2ce0aa8bdc56abb2d6b3`)
-    const {items} = await res.json()
+    const res = await fetch(`https://cdn.contentful.com/spaces/4pywjkutx049/environments/master/entries?access_token=a70d2276fb3f46ebc664b8aeab91d5cc7ee6ef7f9b6d2ce0aa8bdc56abb2d6b3&content_type=blogPost`)
+    const { items } = await res.json()
 
     items.forEach(post => {
       paths[`/post/${post.fields.slug}`] = { page: '/post/[slug]', query: post.fields.slug }
-    });
+    })
 
     return paths
   }
