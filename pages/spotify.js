52dcaf72611752245f417684efc4199b17f8f191
Jacob Arriola <jacob@zeek.com>
2019-10-23 07:02:46 -0700

    key

--- pages/spotify.js
+++ pages/spotify.js
@@ -1,34 +1,38 @@
-import Layout from '../components/layout'
-import { useState } from 'react'
-import fetch from 'isomorphic-unfetch'
-
+import Layout from "../components/layout";
+import { useState } from "react";
+import fetch from "isomorphic-unfetch";
 
 const Spotify = () => {
-  const [query, setQuery] = useState('')
-  const [items, setItems] = useState({items: []})
+  const [query, setQuery] = useState("");
+  const [items, setItems] = useState({ items: [] });
 
   const handleSearchChange = event => {
-    setQuery(event.target.value)
-  }
+    setQuery(event.target.value);
+  };
 
   const handleSubmission = event => {
-    event.preventDefault()
-    fetchData()
-  }
+    event.preventDefault();
+    fetchData();
+  };
 
   const fetchData = async () => {
-    const res = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=playlist&limit=12`, {headers: {
-        'Authorization': `Bearer BQD53IhQ4wQgdQW3bwlbnKnLYdG6mHSfnDSopPDypiY9vC1rtTXIFyqEO3-gPFjnvEyPRAUtYJWj0aLHmg8`
-      }})
-
-      const json = await res.json()
-
-      if (json.error && json.error.status === 401) {
-        return alert(`Token expired`)
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${query}&type=playlist&limit=12`,
+      {
+        headers: {
+          Authorization: `Bearer BQAKrcfAMnzgdIBkwonQBaTrhXM-QKpjOuIhyyeK55ymzwHa7abYATdTwFv4Quo9OL502ogSVded4ymhuX4`
+        }
       }
-    
-      setItems(json.playlists)
-  }
+    );
+
+    const json = await res.json();
+
+    if (json.error && json.error.status === 401) {
+      return alert(`Token expired`);
+    }
+
+    setItems(json.playlists);
+  };
 
   return (
     <Layout pageTitle="Spotify">
@@ -47,42 +51,39 @@ const Spotify = () => {
         <button type="submit">Search</button>
       </form>
       <ul>
-        {
-          items.items.map(item => (
-            <li key={item.id}>
-              <img src={item.images[0].url} />
-              <h2>{item.name}</h2>
-            </li>
-          ))
-        }
+        {items.items.map(item => (
+          <li key={item.id}>
+            <img src={item.images[0].url} />
+            <h2>{item.name}</h2>
+          </li>
+        ))}
       </ul>
       <style jsx>{`
+        form {
+          margin-bottom: 2.5rem;
+        }
 
-      form {
-        margin-bottom: 2.5rem;
-      }
+        ul {
+          list-style: none;
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          margin: -1.25rem 0 0 -1.25rem;
+          padding-left: 0;
+        }
 
-      ul {
-        list-style: none;
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        margin: -1.25rem 0 0 -1.25rem;
-        padding-left: 0;
-      }
+        img {
+          max-width: 100%;
+          height: auto;
+        }
 
-      img {
-        max-width: 100%;
-        height: auto;
-      }
-
-      li {
-        margin: 1.25rem 0 0 1.25rem;
-        width: calc(33.3333% - 1.25rem);
-      }
-  `}</style>
+        li {
+          margin: 1.25rem 0 0 1.25rem;
+          width: calc(33.3333% - 1.25rem);
+        }
+      `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Spotify
+export default Spotify;
